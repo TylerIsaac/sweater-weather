607191a3be4541ea8e483a14b05a6005a75c740f
tiisaac@comcast.net <tyler.isaac@cochran.com>
2022-02-10 17:00:23 -0500

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "./app.css";
+
 const api = {
   key: "002fc78680218d7154b13ac763cd7f41",
   base: "https://api.openweathermap.org/data/2.5/",
@@ -20,37 +22,55 @@ function App() {
     }
   };
 
-  
-
   return (
-    <div className="App">
-      <main>
-        <div>
-          <input
-            type="text"
-            placeholder="Location..."
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
+    <main className="bg-img">
+      <div className="">
+        <div className=" mx-4 my-4 box blur">
+          <div>
+            <input
+              className="input is-rounded column is-half"
+              type="text"
+              placeholder="Location..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={search}
+              required
+            />
+          </div>
         </div>
+      </div>
 
+      <div className="">
         {typeof weather.main != "undefined" ? (
           <div>
-            <div>
-              <div>{weather.name}, {weather.sys.country}</div>
-              <div>Date</div>
+            <div className="mx-6 my-4 box bg-blue has-text-centered overlay">
+              <div className="is-size-6 has-text-black">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="is-size-6 has-text-black">Date</div>
             </div>
 
-            <div>
-              <div>{Math.round(weather.main.temp * 9/5 + 32)}°F</div>
-            </div>
+            <div className=" mx-4 mt-6 box blur has-text-centered">
+              <div className="columns">
+                <div className="column temp has-text-weight-bold">
+                  {Math.round((weather.main.temp * 9) / 5 + 32)}°F
+                </div>
+                <img
+                  className="column"
+                  src="https://i.imgur.com/n28CWGf.png"
+                  alt="storm-cloud"
+                  width="50%"
+                />
+              </div>
 
-            <div>Raining</div>
+              <div>Raining</div>
+            </div>
           </div>
-        ) : (" ")}
-      </main>
-    </div>
+        ) : (
+          " "
+        )}
+      </div>
+    </main>
   );
 }
 
